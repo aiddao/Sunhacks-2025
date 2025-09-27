c4163b5c01760078ddfcd0c0607772f8661746d0
Aiden Dao <2ndtry91@gmail.com>
2025-09-27 13:58:09 -0700

    Made exercises, reps, weight, and sets save locally

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
+const exerciseList = [];
+const setsList = [];
+const repetitionsList = [];
+const weightList = [];
+
+if(localStorage.exerciseList) {
+    exerciseList.push(...JSON.parse(localStorage.exerciseList));
+}
+if(localStorage.setsList) {
+    setsList.push(...JSON.parse(localStorage.setsList));
+}
+if(localStorage.repetitionsList) {
+    repetitionsList.push(...JSON.parse(localStorage.repetitionsList));
+}
+if(localStorage.weightList) {
+    weightList.push(...JSON.parse(localStorage.weightList));
+}
+
 function recordWorkout(){
-    const exercise = document.getElementById("exercise").value;
-    const sets = document.getElementById("sets").value;
-    const repetitions = document.getElementById("repetitions").value;
-    const weight = document.getElementById("weight").value;
+    event.preventDefault();
+    let exercise = document.getElementById("exercise").value;
+    let sets = document.getElementById("sets").value;
+    let repetitions = document.getElementById("repetitions").value;
+    let weight = document.getElementById("weight").value;
+
+    exerciseList.push(exercise);
+    setsList.push(sets);
+    repetitionsList.push(repetitions);
+    weightList.push(weight);
+    localStorage.setItem("exerciseList", JSON.stringify(exerciseList));
+    localStorage.setItem("setsList", JSON.stringify(setsList));
+    localStorage.setItem("repetitionsList", JSON.stringify(repetitionsList));
+    localStorage.setItem("weightList", JSON.stringify(weightList));
 
     console.log(`Exercise: ${exercise}, Sets: ${sets}, Repetitions: ${repetitions}, Weight: ${weight}`);
+    console.log(setsList);
 }
